9a32d4b9d42517021ff64a4010baab9b8c946dd6
AshwiniKumar1103 <ashwini.kumar1fdbeb@cognizant.com>
2024-10-09 13:39:44 +0530

    Personal Information List

---
+++ LocalStorageConcepts/LocalStorage.js
@@ -0,0 +1,78 @@
+let GetItem = document.getElementById("GetData");
+let SetData = document.getElementById("AddData");
+let Form = document.getElementById("form");
+let setDataInList = document.getElementById("data");
+let data = {};
+let counter = 0;
+const GetDataInLocalHost = () => {
+  return localStorage.getItem("ToDoList");
+};
+let localStorageData = JSON.parse(GetDataInLocalHost()) || [];
+
+const DataWhenPageLoadsFirst = () => {
+  if (localStorageData) {
+    console.log(typeof localStorageData);
+    setDataInList.innerHTML = localStorageData.map((x, index) => {
+      console.log(x);
+      return `<li id=${index}><span>${x.name}\</span> <span>${x.PhNumber}</span> <span>${x.ID}</span> <button id="DeleteData"onclick="DeleteDataInTable(${index})">Delete</button></li>`;
+    });
+  }
+};
+
+const setData = () => {
+  if (
+    (data.name !== undefined || !null) &&
+    (data.PhNumber !== undefined || !null) &&
+    (data.ID !== undefined || !null)
+  ) {
+    if (data) {
+      localStorageData.push(data);
+      localStorageData = [...new Set(localStorageData)];
+      localStorage.setItem("ToDoList", JSON.stringify(localStorageData));
+      console.log(localStorageData);
+      DataWhenPageLoadsFirst();
+    }
+  } else {
+    alert("you missed input boxes");
+  }
+};
+
+const formChange = (e) => {
+  if (e.target.name === "name") {
+    data = { ...data, name: e.target.value };
+  } else if (e.target.name === "PhNumber") {
+    data = { ...data, PhNumber: e.target.value };
+  } else if (e.target.name === "ID") {
+    data = { ...data, ID: e.target.value };
+  }
+  console.log(data);
+};
+
+const DeleteDataInTable = (e) => {
+  console.log(e);
+  localStorageData = localStorageData.filter((item, index) => {
+    console.log(`${index}  ${e}`);
+    if (e !== 0) {
+      if (index !== e) {
+        return item;
+      }
+    } else {
+      if (index !== 0) {
+        return item;
+      }
+    }
+  });
+  console.log(localStorageData);
+  localStorage.setItem("ToDoList", JSON.stringify(localStorageData));
+  DataWhenPageLoadsFirst();
+};
+
+(() => {
+  GetDataInLocalHost();
+  console.log(localStorageData);
+  DataWhenPageLoadsFirst();
+})();
+
+Form.addEventListener("change", formChange);
+SetData.addEventListener("click", setData);
+//GetItem.addEventListener("click", getItem);
